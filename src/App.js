a42a22f017354fdb59956fb7317a91a005fc8553
viveknhc <viveknhc@gmail.com>
2023-07-18 11:25:24 +0530

    18/07/23

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
 
 function App() {
+  const [advice, setAdvice] = useState("")
+  const [count, setCount] = useState(0)
+  async function getData() {
+    const resurl = await fetch("https://api.adviceslip.com/advice")
+    const data = await resurl.json()
+    setAdvice(data.slip.advice)
+    setCount((c) => c + 1)
+  }
+
+  useEffect(function () {
+    getData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Advice App</h1>
+      <p>Click the button then you can get the advice</p>
+      <button onClick={getData}>Click Here</button>
+
+      <h3>{count}:{advice}</h3>
+
     </div>
-  );
+  )
+
 }
 
-export default App;
+export default App
+
